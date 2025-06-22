8032b67c8778e7ca385223610ab0c12cdc99c649
Volodymyr Morhun <your.email@example.com>
2025-06-22 22:02:21 +0300

    birthday

--- birthday.py
+++ birthday.py
@@ -1,4 +1,6 @@
 from collections import UserDict
+import re
+from datetime import datetime 
 
 class Field:
     def __init__(self, value):
@@ -18,17 +20,6 @@ class Phone(Field):
         if not value.isdigit() or len(value)!=10:
             raise ValueError('Phone number must consist')
         super().__init__(value)
-
-class Record:
-    def __init__(self, name):
-        self.name = Name(name)
-        self.phones = []
-        self.birthday = None
-
-    # реалізація класу
-
-    def __str__(self):
-        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
 
 class AddressBook(UserDict):
     # реалізація класу
@@ -53,9 +44,14 @@ class Record:
     def __init__(self, name):
         self.name=Name(name)
         self.phones=[]
+        self.birthday = None
     
     def add_phone(self, phone_number):
         self.phones.append(Phone(phone_number))
+
+    def add_birthday(self, day):
+        self.birthday=Birthday(day)
+
 
     def remove_phone(self, phone_number):
         for phone in self.phones:
@@ -63,17 +59,20 @@ class Record:
                 self.phones.remove(phone)
                 return True
         return False
+    
     def edit_phone(self, old_phone_number, new_phone_number):
         for inx, phone in enumerate(self.phones):
             if phone.value==old_phone_number:
-                self.phones[inx]==Phone(new_phone_number)
+                self.phones[inx]=Phone(new_phone_number)
             return
         raise ValueError ('old number not found')
+    
     def find_phone(self, phone_number):
         for phone in self.phones:
             if phone.value==phone_number:
                 return phone
         return None
+    
     def __str__(self):
             
         phone_str= "; ".join(phone.value for phone in self.phones)
@@ -84,14 +83,48 @@ class Record:
 
 class Birthday(Field):
     def __init__(self, value):
+        data_format="%d.%m.%Y"
         try:
-            # Додайте перевірку коректності даних
+            datetime.strptime(value, data_format)
             # та перетворіть рядок на об'єкт datetime
         except ValueError:
             raise ValueError("Invalid date format. Use DD.MM.YYYY")
+        super().__init__(value)
 
 
 
+# Створення нової адресної книги
+book = AddressBook()
+
+# Створення запису для John
+john_record = Record("John")
+john_record.add_phone("1234567890")
+john_record.add_phone("5555555555")
+john_record.add_birthday("12.12.2024")
+# Додавання запису John до адресної книги
+book.add_record(john_record)
+
+# Створення та додавання нового запису для Jane
+jane_record = Record("Jane")
+jane_record.add_phone("9876543210")
+book.add_record(jane_record)
+
+# Виведення всіх записів у книзі
+    
+print(book)
+
+# Знаходження та редагування телефону для John
+john = book.find("John")
+john.edit_phone("1234567890", "1112223333")
+
+print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
+
+# Пошук конкретного телефону у записі John
+found_phone = john.find_phone("5555555555")
+print(f"{john.name}: {found_phone}")  # Виведення: John: 5555555555
+
+# Видалення запису Jane
+book.delete("Jane")
+    
         
-          
 
